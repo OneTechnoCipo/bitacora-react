528e0c28c0ff8d7e850aec80d246e84be41b9a48
OneTechnoCipo <marianocollazo2003@gmail.com>
2026-04-15 16:43:39 -0300

    init: empty react and typescript project

---
+++ bitacora-react/src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+
+// clean start for the bitacora
+// i deleted all the vite default stuff to start fresh
+function App() {
+  return (
+    <div>
+      <h1>Bitacora de Consumo (React Version)</h1>
+
+    </div>
+  )
+}
+
+export default App
